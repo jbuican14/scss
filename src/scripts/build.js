20b113f9983e7b0347e8f14dc3e5198b9417be1c
jbuican14 <jbuican19@gmail.com>
2024-06-24 22:04:54 +0100

    compile function to compile components to css

--- src/scripts/build.js
+++ src/scripts/build.js
@@ -2,10 +2,38 @@ const Fs = require('fs');
 const Path = require('path');
 const Sass = require('node-sass');
 
-const result = Sass.renderSync({
-  file: Path.resolve('src/global.scss'),
-  includePaths: [Path.resolve('src')],
-});
+// const result = Sass.renderSync({
+//   file: Path.resolve('src/global.scss'),
+//   includePaths: [Path.resolve('src')],
+// });
+
+/*----------  getComponents to define the individual component  ----------*/
+
+const getComponents = () => {
+  let allComponents = [];
+
+  const types = ['atoms', 'molecules', 'organisms'];
+
+  types.forEach((type) => {
+    const allFiles = Fs.readdirSync(`src/${type}`).map((file) => ({
+      input: `src/${type}/${file}`,
+      output: `src/lib/${file.slice(0, -4) + 'css'}`,
+    }));
+
+    allComponents = [...allComponents, ...allFiles];
+  });
+
+  return allComponents;
+};
+
+const compile = (path, fileName) => {
+  const result = Sass.renderSync({
+    file: Path.resolve(path),
+  });
+  Fs.writeFileSync(Path.resolve(fileName), result.css.toString());
+};
+
+compile('src/global.scss', 'src/lib/global.css');
 
 // deprecated
 // const s = Sass.renderSync({
@@ -16,4 +44,9 @@ const result = Sass.renderSync({
 // });
 
 // console.log(result.css.toString());
-Fs.writeFileSync(Path.resolve('src/lib/global.css'), result.css.toString());
+
+// console.log(getComponents());
+
+getComponents().forEach((component) => {
+  compile(component.input, component.output);
+});
